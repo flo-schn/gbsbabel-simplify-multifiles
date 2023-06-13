88e5e057fd6e0e265c1b084bdab110aa8a376567
098714 <fschn@fschn.org>
2023-06-13 09:32:55 +0200

    feature drop

--- script.sh
+++ script.sh
@@ -1,9 +1,36 @@
 #! /bin/bash
 
-for i in `find . -name "*.gpx"`
+for gpx in `find . -name "*.gpx"`
 do
-test=$(grep -wc "</trkpt>" $i)
-test2=$(($test / 10))
-echo $i has $test trackpoints and will be reduced to $test2
-gpsbabel -i gpx -f $i -x simplify,count=$test2 -o gpx -F ${i:2:(-4)}-reduced-$test2.gpx
+len=$(grep -wc "</trkpt>" $gpx)
+echo $gpx has $len trackpoints
 done
+
+echo
+echo 'To reduce number of trackpoints to eg half or quater of initial value, enter integer of 2 or 4: '
+read fraction 
+echo
+
+for gpx in `find . -name "*.gpx"`
+do
+len=$(grep -wc "</trkpt>" $gpx)
+newLen=$(($len / $fraction))
+echo $gpx has $len trackpoints and will be reduced to $newLen
+done
+
+echo
+read -p "Procede to reduce gpx tracks to 1/$fraction? [Y/n] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    for gpx in `find . -name "*.gpx"`
+    do
+    len=$(grep -wc "</trkpt>" $gpx)
+    newLen=$(($len / $fraction))
+    gpsbabel -i gpx -f $gpx -x simplify,count=$newLen -o gpx -F ${gpx:2:(-4)}-reduced-$newLen.gpx
+    echo ${gpx:2:(-4)}-reduced-$newLen.gpx
+    done
+fi
+
+
